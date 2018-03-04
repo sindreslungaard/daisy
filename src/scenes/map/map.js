f5f4c93e262f06a058ee9aca29efc44aac1982c3
Sindre Slungaard <slungaards@gmail.com>
2018-03-04 14:54:39 +0100

    scene and map related updates

---
+++ src/scenes/map/map.js
@@ -0,0 +1,7 @@
+export default class Map {
+
+  constructor(data) {
+    this.data = data;
+  }
+
+}
--- src/scenes/scene.js
+++ src/scenes/scene.js
@@ -3,6 +3,18 @@ export default class Scene {
   constructor(canvas_id) {
     this.render = true;
     this.canvas = document.getElementById(canvas_id);
+    this.context = this.canvas.getContext("2d");
+  }
+
+
+  // Adjust the width and height of the canvas
+  adjust(w, h) {
+    this.canvas.width = w ? w : window.innerWidth;
+    this.canvas.height = h ? h : window.innerHeight;
+  }
+
+  createMap() {
+
   }
 
 
