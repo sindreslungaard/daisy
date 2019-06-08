fb08d173af1174b7ed28c739abb161a94207edcd
Sindre Slungaard <slungaards@gmail.com>
2019-06-08 21:01:09 +0200

    export pixi references for type checking

--- src/daisy.ts
+++ src/daisy.ts
@@ -4,6 +4,7 @@ import * as translate from './utils/translate'
 
 export const Events = EventRegistry
 export const Translate = translate
+export { Loader, Texture, BaseTexture, Sprite } from 'pixi.js'
 export { default as App } from './core/App'
 export { default as preload } from './utils/preload'
 export { default as Scene } from './core/containers/Scene'
